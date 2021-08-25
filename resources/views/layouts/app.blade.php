052ac5bd0efefa9b1e8ca2ced75ff85f59d97d24
PatrickMamsery <pkrobert1612@gmail.com>
2021-08-25 11:52:28 +0300

    feat: Initially add gallery feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <title>St. Augustine Family</title>
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            {{-- <li>
+                <a href="" class="p-3">
+                    <img src="/img/logo.png" width="50" height="50" alt="">
+                </a>
+            </li> --}}
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Gallery</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
+
+    <script src="https://unpkg.com/colcade@0/colcade.js"></script>
+
+    <script>
+        var colc = new Colcade( '.grid', {
+            columns: '.grid-col',
+            items: '.grid-item'
+        });
+    </script>
 </body>
 </html>
