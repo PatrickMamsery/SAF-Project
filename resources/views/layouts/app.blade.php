0d1cf0ec48752b3fee4508c72737bdfe8194792c
PatrickMamsery <pkrobert1612@gmail.com>
2021-11-26 12:24:31 +0300

    fix: asset allocation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     {{-- <link rel="stylesheet" href="/css/bootstrap.min.css"> --}}
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
     <link rel="stylesheet" href="/css/snackbar.min.css">
     <script src="/packages/snackbar/dist/snackbar.min.js"></script>
     <title>St. Augustine Family</title>
@@ -32,7 +32,7 @@
 
 
     <script src="https://unpkg.com/colcade@0/colcade.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 
     <script>
         var colc = new Colcade( '.grid', {
