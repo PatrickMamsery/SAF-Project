05751099e4899e7d335967aec367bcf2b236f41a
PatrickMamsery <pkrobert1612@gmail.com>
2022-09-08 08:58:42 +0300

    fix: add site down markup

---
+++ resources/views/site_down.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>St. Augustine Choral Society of Tanzania</title>
+</head>
+<body>
+    
+    <div class="container-fluid site-down">
+        <div class="row text-center justify-content-center p-5">
+            <div class="info">
+                <h1>St. Augustine Choral Society of Tanzania</h1>
+                <p>Website is currently under maintenance. Please check back later.</p>
+            </div>
+    
+            <div class="social">
+                <ul>
+                    <li><a href="https://web.facebook.com/Kwaya-ya-MtAugustino-Parokia-ya-chuo-kikuu-110038991299260/">
+                        <img src="/icons/facebook.svg" alt="">
+                    </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/st_augustine_choir_udsm/">
+                            <img src="/icons/instagram.svg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCxSb1bv9YHMR7eYUHSRu-1w">
+                            <img src="/icons/youtube.svg" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+</body>
+</html>
